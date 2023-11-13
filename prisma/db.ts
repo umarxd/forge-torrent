914f0f2d460c66f2f85e9d3fd4d2f5eb32824207
umarxd <xqcowsavedmylife@gmail.com>
2023-11-13 21:08:49 +0300

    fix prisma setup

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,21 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import "server-only";
 
 declare global {
-  // eslint-disable-next-line no-var, no-unused-vars
-  var cachedPrisma: PrismaClient;
+  var prisma: PrismaClient;
 }
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
+// Check if PrismaClient is defined. This helps with hot-reloading in development.
+if (process.env.NODE_ENV === "development" && !global.prisma) {
+  global.prisma = new PrismaClient();
 }
 
-const db = prisma;
+const db = global.prisma || new PrismaClient();
 
 export default db;
